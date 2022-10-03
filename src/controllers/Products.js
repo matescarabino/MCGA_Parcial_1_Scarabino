1b014a111d8e105ef93790e04c6e0455806888e2
Mateo Scarabino <matescarabino@gmail.com>
2022-10-03 15:34:03 -0300

    MCGA-05: Create update and delete methods

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -1,20 +1,18 @@
 const Products = require('../models/products');
 
-
-
 //Ping
 const ping = (req, res) => {
-        res.send("DB Connected!");
+        res.send("OK. DB Connected!");
 };
 
-//Get All Products
+//Get All
 const getAll = (req, res) => {
     Products.find()
         .then((data) => res.status(200).json({ msg: "All Products", data, error: false }))
         .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
 };
 
-//Get by Product Id
+//Get
 const getId = (req, res) => {
     const {_id} = req.params;
     Products.findById(parseInt(_id))
@@ -22,7 +20,7 @@ const getId = (req, res) => {
         .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
 };
 
-// Post new Product
+// Post
 const postProduct = (req, res) => {
     const { _id, name, price, stock, description } = req.body;
     const newProduct = { _id, name, price, stock, description };
@@ -30,10 +28,35 @@ const postProduct = (req, res) => {
         .then((data) => res.status(201).json({ msg: "Product added: ", data, error: false }))
         .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
 };
+
+// Update
+const updateProduct = (req, res) => {
+    const { _id } = req.params;
+    Products.findByIdAndUpdate(parseInt(_id), req.body, { new: true })
+        .then((data) => {
+            if (data.length === 0) return res.status(404).json({ msg: `Product not found by ID: ${_id}`, data: {}, error: true });
+            return res.status(202).json({ msg: "Product updated", data, error: false });
+        })
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
+};
+
+// Delete
+const deleteProduct = (req, res) => {
+    const { _id } = req.params;
+    Products.findByIdAndUpdate(parseInt(_id), {isDeleted: true})
+        .then((data) => {
+            if (data.length === 0) return res.status(404).json({ msg: `Product not found by ID: ${id}`, data: {}, error: true });
+            return res.status(202).json({ msg: "Product deleted", data, error: false });
+        })
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
+};
+
 // Exports
 module.exports = {
     ping,
     getAll,
     getId,
-    postProduct
+    postProduct,
+    updateProduct,
+    deleteProduct
 };
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {ping, getAll, getId, postProduct} = require('../controllers/Products.js');
+const {ping, getAll, getId, postProduct, updateProduct, deleteProduct} = require('../controllers/Products.js');
 
 //Ping DB
 router.get('/',ping);
@@ -13,5 +13,11 @@ router.get('/product/:_id',getId);
 
 //Post new Product
 router.post('/product/add',postProduct);
+
+//Update Product
+router.put('/product/update/:_id',updateProduct);
+
+//Delete Product
+router.delete('/product/delete/:_id',deleteProduct);
 
 module.exports=router;
