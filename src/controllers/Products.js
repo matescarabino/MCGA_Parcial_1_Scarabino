36f573cf1ea1f3fa3d1cff5feed24ce04f98783b
Mateo Scarabino <matescarabino@gmail.com>
2022-10-03 21:50:49 -0300

    MCGA-08: Fix getId  method

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -7,8 +7,9 @@ const ping = (req, res) => {
 
 //Get All
 const getAll = (req, res) => {
-    Products.find()
+    Products.find({isDeleted: false})
         .then((data) => res.status(200).json({ msg: "All Products", data, error: false }))
+
         .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
 };
 
